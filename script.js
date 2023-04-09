c8691cb3781c8c402d31fa5405f9d24ab282ab0b
Obaayaa-1988 <aquaicoe31@gmail.com>
2023-04-09 17:16:25 +0100

    added if statement for comparing first and last letters

--- script.js
+++ script.js
@@ -11,11 +11,20 @@ function logicPalindrome(){
     //picking the first two letters of the word typed into the input field
 
     let startWord = word.substring(0, Math.floor(len / 2)).toLowerCase();
-    alert(startWord);
+    // alert(startWord);
 
     let endWord = word.substring(len - Math.floor(len / 2)).toLowerCase();
     //this is for reversing the last two letters to it looks the same as the first two letters(startword) 
-    let flip = [...endWord].reverse().join("")
+    let flipEnd = [...endWord].reverse().join("")
+
+    //comparing the startword to flipEnd to check if they are the same
+
+    if(startWord == flipEnd){
+        result.innerHTML = `${word.toUpperCase()} is a Palindrome`
+    }else{
+        result.innerHTML = `${word.toUpperCase()} is not a Palindrome`
+
+    }
 
 
 }
