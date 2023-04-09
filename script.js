c6aef4a9a6bdf874d48c59f867cb68e91822d922
Obaayaa-1988 <aquaicoe31@gmail.com>
2023-04-09 17:06:46 +0100

    adding logic for selecting first and last two letters

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 const btn = document.querySelector(".btn");
 const result = document.querySelector(".result");
-const word = document.querySelector(".input-text").value;
 
+btn.addEventListener("click", logicPalindrome);
+
+function logicPalindrome(){
+    const word = document.querySelector(".input-text").value;
+    //finding the length of the word typed into the input field
+    let len = word.length;
+
+    //picking the first two letters of the word typed into the input field
+
+    let startWord = word.substring(0, Math.floor(len / 2)).toLowerCase();
+    alert(startWord);
+
+    let endWord = word.substring(len - Math.floor(len / 2)).toLowerCase();
+    //this is for reversing the last two letters to it looks the same as the first two letters(startword) 
+    // let flip = [...endWord].reverse().join("")
+
+
+}
+
+// alert("hello")
